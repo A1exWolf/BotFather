e67f86b3963aba26e3f16fc672ea5c8afc2c7974
Sasha Volkov <a1exs.wolf@yandex.ru>
2023-02-04 14:53:02 +0300

    correct files

--- first/nums.py
+++ first/nums.py
@@ -1,12 +1,13 @@
-def plus(a,b):
+def plus(a, b):
     return a+b
 
-def mul(a,b):
+
+def mul(a, b):
     return a*b
 
-def minus(a,b):
-    return a-b
 
+def minus(a, b):
+    return a-b
 
 
 if __name__ == '__main__':
